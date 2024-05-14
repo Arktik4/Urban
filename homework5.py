38cd0a8ccc541a28338d39cd43f5de27cd453e97
Arktik4 <121449128+Arktik4@users.noreply.github.com>
2024-05-14 16:49:33 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,18 @@
+my_list = ['car', 'moto', 'bus', 'train', 'metro']
+print('List: ', my_list)
+print('First element: ', my_list[0])
+print('Last element: ', my_list[-1])
+print('Sublist: ', my_list[2:5])
+
+my_list[2] = 'autobus'
+print('Modified list: ', my_list)
+
+print()
+
+my_dict = {'Один': 1, 'Два': 2, 'Три': 3, 'Четыре': 4, 'Пять': 5}
+print('Счет: ', my_dict)
+print('Двойка: ', my_dict['Два'])
+my_dict['Шесть'] = 6
+print('Новый счет: ', my_dict)
+
+
