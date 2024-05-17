09c02529c5f9a1ec0872484b598e03706f712479
Arktik4 <121449128+Arktik4@users.noreply.github.com>
2024-05-17 18:26:36 +0700

    Add files via upload

---
+++ main_3.py
@@ -0,0 +1,13 @@
+def test(a, b):
+    print(a, b)
+
+def test_2(a, b, c):
+    print(a, b, c)
+
+test(5, 'five')
+test('Дай', 'пять')
+
+print()
+
+test_2(3, 'три', 'three')
+test_2('и', 'получишь', 'дырку')
